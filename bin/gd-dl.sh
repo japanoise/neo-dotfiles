01534a16da51bda12345a6c7fdad1b385060d5cc
japanoise <japanoise@tfwno.gf>
2018-12-17 16:34:23 +0000

    Use read -r

--- bin/gd-dl.sh
+++ bin/gd-dl.sh
@@ -7,6 +7,6 @@ wget_args=--user-agent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/2
 
 wget "$wget_args" -O - "$URL" 2>/dev/null | sed -n -e 's/[^"]*"\(\/download\/[^"]*vbr.m3u\).*/https:\/\/archive.org\1/p' | xargs wget "$wget_args" -O "$TMPDIR/files.txt"
 
-while read -u 10 p; do
+while read -r -u 10 p; do
 	wget "$wget_args" "$p"
 done 10<"$TMPDIR/files.txt"
