a158e59f0b0229ef4523d33dee0463379128d7d4
japanoise <japanoise@seekrit.club>
2022-05-29 12:46:35 -0500

    Switch from i3lock to xsecurelock

--- bin/japlock.sh
+++ bin/japlock.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dunstctl set-paused true
-i3lock -n
+XSECURELOCK_LIST_VIDEOS_COMMAND='find -L ~/vid/saver -type f' XSECURELOCK_SAVER=saver_mpv XSECURELOCK_NO_COMPOSITE=1 xsecurelock || i3lock -n
 dunstctl set-paused false
