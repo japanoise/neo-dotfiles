2aca8194ca7d611d449a9e3792c84c0558cb0853
japanoise <japanoise@seekrit.club>
2022-06-05 17:08:47 -0500

    Add newlines to help output

--- bin/submod.sh
+++ bin/submod.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # Remember all the stupid submodule commands for me
 usage() {
-    printf "%s fetch - fetch submodules, if you forgot to recursive clone" \
+    printf "%s fetch - fetch submodules, if you forgot to recursive clone\n" \
            "$0"
-    printf "%s update [submodule] - update given submodule" \
+    printf "%s add [url] - add new submodule\n" \
            "$0"
-    printf "If you're more patient than I am: %s" \
+    printf "%s update [submodule] - update given submodule\n" \
+           "$0"
+    printf "If you're more patient than I am: %s\n" \
            "https://git-scm.com/book/en/v2/Git-Tools-Submodules"
 }
 
