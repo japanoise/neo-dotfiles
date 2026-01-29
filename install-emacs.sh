41188420b5f64c7148f18f38ffa831da0348a56e
japanoise <japanoise@seekrit.club>
2026-01-29 15:29:34 -0600

    [install-emacs] Add XDG_CONFIG_HOME support; add openrc support

--- install-emacs.sh
+++ install-emacs.sh
@@ -17,12 +17,18 @@ git clone ssh://git@github.com/japanoise/neo-dotemacs "$HOME"/.emacs.d
 echo "first run, exit once packages are installed (C-x C-c)"
 emacs
 
-echo "checking for systemd..."
+configDir="$XDG_CONFIG_HOME"
+if [ -z "$configDir" ]
+then
+    configDir="$HOME"/.config
+fi
+
+echo "checking init..."
 if file "$(which init)" | grep -q systemd
 then
 	echo "installing systemd startup hook..."
-	mkdir -pv "$HOME"/.config/systemd/user/
-	cat > "$HOME"/.config/systemd/user/emacsd.service <<EOF
+	mkdir -pv "$configDir"/systemd/user/
+	cat > "$configDir"/systemd/user/emacsd.service <<EOF
 [Unit]
 Description=Emacs: the extensible, self-documenting text editor
 Documentation=man:emacs(1) info:Emacs
@@ -42,10 +48,34 @@ EOF
 	systemctl --user start emacsd
 	echo "OK, if that all went well, you should now have an emacs server running."
 	echo "'systemctl --user restart emacsd' to restart it."
+elif file "$(which init)" | grep -q openrc
+then
+    echo "installing openrc user script..."
+    mkdir -pv "$configDir"/rc/init.d/
+    cat > "$configDir"/rc/init.d/emacsd <<EOF
+#!/sbin/openrc-run
 
+name=$RC_SVCNAME
+description="Emacs: the extensible, self-documenting text editor"
+command="/usr/bin/emacs"
+command_args="--daemon"
+pidfile="undefined"
+
+stop() {
+	ebegin "Stopping $RC_SVCNAME"
+	/usr/bin/emacsclient --eval "(progn (setq kill-emacs-hook nil) (kill-emacs))"
+	eend $?
+}
+EOF
+    chmod +x "$configDir"/rc/init.d/emacsd
+    rc-update --user add emacsd default
+    rc-service --user emacsd start
+    echo "OK, if that all went well, you should now have an emacs server running."
+    echo "'rc-service --user emacsd restart' to restart it."
 else
-	echo "congratulations, your system isn't infected!"
-	echo "however, this means you're on your own installing the init script."
+	echo "congratulations, your system isn't infected with systemd!"
+	echo "unfortunately, you're not using openrc either."
+	echo "this means you're on your own installing the init script."
 	echo "the start command should be:"
 	echo "/usr/bin/emacs --daemon"
 	echo "and the stop command:"
